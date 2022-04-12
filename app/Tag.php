dc81beceaf711c9b62259e5dd0893883ea67d6ae
katiepalumbo <palumbo.katie@gmail.com>
2022-04-12 21:12:11 +0200

    created migrations and tag model

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = ['tag_name', 'tag_slug'];
+
+    public function posts() {
+        return $this->belongsToMany('App\Post');
+    }
 }
